48df06bb3a4f5711dff273b99e59dfe53da00dd9
Xihai Luo <xihai.luo01@gmail.com>
2022-02-14 11:28:46 -0500

    wrote some simple prisma queries

--- script.ts
+++ script.ts
@@ -12,14 +12,33 @@ async function main() {
       email: "alice@prisma.io"
     }
   }); */
-  const updateUser = await prisma.user.update({
+ /*  const updateUser = await prisma.user.update({
     data: {
       name: "Alice"
     },
     where: {
       email: "alice@prisma.io"
     }
+  }); */
+  /* const newPost = await prisma.post.create({
+    data: {
+      title: "Hello World"
+    }
   });
+  console.log(newPost); */
+  const result = await prisma.post.update({
+    where: {
+      id: 1
+    },
+    data: {
+      author: {
+        connect: {
+          email: "alice@prisma.io"
+        }
+      }
+    }
+  });
+  console.log(result);
 }
 
 main()
