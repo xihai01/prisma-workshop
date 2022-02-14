eb9371d62c8691a54c976ded1d4a07fe1ba8b34d
Xihai Luo <xihai.luo01@gmail.com>
2022-02-14 11:21:51 -0500

    add Post table

--- script.ts
+++ script.ts
@@ -5,6 +5,21 @@ const prisma = new PrismaClient()
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
+  /* const users = await prisma.user.findMany();
+  console.table(users); */
+  /* const newUser = await prisma.user.create({
+    data: {
+      email: "alice@prisma.io"
+    }
+  }); */
+  const updateUser = await prisma.user.update({
+    data: {
+      name: "Alice"
+    },
+    where: {
+      email: "alice@prisma.io"
+    }
+  });
 }
 
 main()
