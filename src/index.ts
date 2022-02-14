44b42d7e1551d4c2985455bc3e9079860f96561e
Xihai Luo <xihai.luo01@gmail.com>
2022-02-14 12:00:36 -0500

    add some rest routes

--- src/index.ts
+++ src/index.ts
@@ -7,48 +7,82 @@ const app = express();
 app.use(express.json());
 
 app.get("/users", async (req, res) => {
-  // const result = TODO
-  // res.json(result)
+  const result = await prisma.user.findMany();
+  res.json(result)
 });
 
 app.post(`/signup`, async (req, res) => {
   const { name, email } = req.body;
+  const result = await prisma.user.create({
+    data: {
+      name: name,
+      email: email
+    }
+  });
 
-  // const result = TODO
-
-  // res.json(result)
+  res.json(result)
 });
 
 app.post(`/post`, async (req, res) => {
   const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: {
+        connect: {
+          email: authorEmail
+        }
+      }
+    }
+  });
 
-  // const result = TODO
-
-  // res.json(result)
+  res.json(result)
 });
 
 app.put("/post/:id/views", async (req, res) => {
   const { id } = req.params;
+  const result = await prisma.post.update({
+    where: {
+      id: Number(id)
+    },
+    data: {
+      viewCount: {
+        increment: 1
+      }
+    }
+  });
 
-  // const result = TODO
-
-  // res.json(result)
+  res.json(result)
 });
 
 app.put("/publish/:id", async (req, res) => {
   const { id } = req.params;
+  const result = await prisma.post.update({
+    where: {
+      id: Number(id)
+    },
+    data: {
+      published: true
+    }
+  });
 
-  // const result = TODO
-
-  // res.json(result)
+  res.json(result)
 });
 
 app.get("/user/:id/drafts", async (req, res) => {
   const { id } = req.params;
+  const result = await prisma.user.findUnique({
+    where: {
+      id: Number(id)
+    }
+  }).posts({
+    where: {
+      published: false
+    }
+  });
 
-  // const result = TODO
-
-  // res.json(result)
+  res.json(result)
 });
 
 app.get(`/post/:id`, async (req, res) => {
